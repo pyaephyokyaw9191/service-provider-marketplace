68aeecb58a21257789bea8e3792ee034f4fc2b41
Azat Siu <umraliyev.azvt@gmail.com>
2025-05-17 10:50:42 +1000

    Moved backend files to the folder

--- auth-service/src/main/java/com/eventorganizer/auth/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.eventorganizer.auth.model;
-
-public enum Role {
-    USER,
-    SERVICE_PROVIDER
-} 
--- payment-service/src/main/java/com/eventorganizer/payment/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventorganizer.payment.repository;
-
-import com.eventorganizer.payment.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
